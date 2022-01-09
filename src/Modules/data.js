ada39fa5fda0ad616b133199de1e90aae859718d
FernandaFA02 <fajardo.al9402@gmail.com>
2022-01-08 21:39:55 -0600

    correción en creación de colección

--- src/Modules/data.js
+++ src/Modules/data.js
@@ -1,5 +1,35 @@
 import * as module from "./firebase.js"
 import { db } from "./firebase.js"
+import {  collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+
+// Comprobando coneccion para crear colecciones
+console.log(db);
+
+const guardarNota = async (titulo, nota, tema) => {
+    try {
+        const docRef = await addDoc(collection(db, "Notas"),{
+            titulo, nota,tema, fecha: new Date (),
+        });
+        console.log("Document written with ID: ", docRef.id);
+    }catch (e) {
+        console.error("Error adding document: ", e);
+    }
+    }
+
+
+//  ----------------------- Se activa el btn de agregar notas para que envie los datos a firebase ------------------//
+
+const agregarNotas = document.getElementById('btn-agregar');
+
+agregarNotas.addEventListener('click', async (e) => {
+    e.preventDefault();
+        const titulo = document.getElementById('titulo').value;
+        const nota = document.getElementById('descripcion').value;
+        const tema = document.getElementById('temas').value;
+        console.log(titulo,nota,tema);
+        await guardarNota(titulo, nota, tema);
+});
+
 
 //---------------------------- Se atrae la data del JSON -------------------------------//
 const traerData = "../data/topics.json";
@@ -29,6 +59,5 @@ const iterarTopics = (arrayTopics) => {
                         <option>${tema.topic}</option>`
     }
 }
-// Comprobando coneccion para crear colecciones
-console.log(db);
+
 
--- src/Modules/firebase.js
+++ src/Modules/firebase.js
@@ -1,10 +1,7 @@
 // ------------------------ Se agrega el SDK del proyecto en firebase ----------------------------------------------//
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore, collection, getDocs, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCtDQ3_boIltWqKVUrVi7CHZffVPpyTWhI",
   authDomain: "notes-app-396ac.firebaseapp.com",
@@ -16,44 +13,3 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-// ------------------------------ Se  crea la colección que enviará las notas ---------------------------------------//
-// try {
-//   const docRef = await addDoc(collection(db, "Notas"), {
-//     first: "Ada",
-//     last: "Lovelace",
-//     born: 1815
-//   });
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
-
-//  ----------------------- Se activa el btn de agregar notas para ue envie los datos a firebase ------------------//
-
-const agregarNotas = document.getElementById('btn-agregar');
-agregarNotas.addEventListener('click', async (e) => {
-    e.preventDefault();
-
-    let notas = {
-        titulo: document.getElementById('titulo').value,
-        nota: document.getElementById('descripcion').value,
-        tema: document.getElementById('temas').value
-    };
-    console.log(notas);
-    await guardarNota(notas);
-    setTimeout(() => {
-        alert('Tú nota ha sido guardada!')
-    }, 1500);
-})
-
-const guardarNota = (notas) => {
-  console.log(notas);
-  db.collection('Notes').doc().set(notas);
-}
-// try {
-//     const guardarNota = await addDoc(collection(db, "Notas").set(notas));
-//     console.log("Document written with ID: ", docRef.id);
-//   } catch (e) {
-//     console.error("Error adding document: ", e);
-//   }
